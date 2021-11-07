35e1492d4e4154cdedf9104c4169fafdaaa69b2b
em-3 <69275008+em-3@users.noreply.github.com>
2021-11-07 16:03:32 -0700

    Optimized fetch() Promise resolution in refreshResults()

--- js/databaseBrowser.js
+++ js/databaseBrowser.js
@@ -14,32 +14,30 @@ function refreshResults () {
     }
 
     //Fetch new results
-    fetch("/api/search" + PHPParams).then(function(response) {
-        response.json().then((data) => {
-            if (data.length === 0) {
-                var noResults = document.createElement("p");
-                noResults.className = "noResults";
-                noResults.textContent = "No results found.";
-                document.querySelector(".resultsContainer").appendChild(noResults);
-            } else {
-                for (i in data) {
-                    var resultElement = document.createElement("div");
-                    resultElement.className = "result";
+    fetch("/api/search" + PHPParams).then(response => response.json()).then((data) => {
+        if (data.length === 0) {
+            var noResults = document.createElement("p");
+            noResults.className = "noResults";
+            noResults.textContent = "No results found.";
+            document.querySelector(".resultsContainer").appendChild(noResults);
+        } else {
+            for (i in data) {
+                var resultElement = document.createElement("div");
+                resultElement.className = "result";
 
-                    var name = document.createElement("h3");
-                    name.classList.add("name");
-                    name.textContent = data[i].name;
+                var name = document.createElement("h3");
+                name.classList.add("name");
+                name.textContent = data[i].name;
 
-                    resultElement.appendChild(name);
-                    document.querySelector(".resultsContainer").appendChild(resultElement);
-                }
+                resultElement.appendChild(name);
+                document.querySelector(".resultsContainer").appendChild(resultElement);
             }
+        }
 
-            //Hide the loading screen and show the database browser
-            document.querySelector(".loadingScreen").classList.add("hidden");
-            document.querySelector(".browser").classList.remove("hidden");
-        });
-    }, function(error) {
+        //Hide the loading screen and show the database browser
+        document.querySelector(".loadingScreen").classList.add("hidden");
+        document.querySelector(".browser").classList.remove("hidden");
+    }, (error) => {
         var errorMessage = document.createElement("p");
         errorMessage.className = "errorMessage";
         errorMessage.textContent = "Something went wrong.";
