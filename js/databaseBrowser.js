04b5a9a7c9c945fc2bf74fc97084494f0a7eca3e
cadehunter <60188691+cadehunter@users.noreply.github.com>
2021-11-07 18:59:37 -0700

    Added styling to databaseBrowser results

--- js/databaseBrowser.js
+++ js/databaseBrowser.js
@@ -1,5 +1,42 @@
 var filters = [];
 
+function fetch() {
+     return new Promise(function (resolve, reject) {
+         resolve(JSON.stringify([
+                {
+                    name: "Splash Mountain at Night",
+                    author: "91JLovesDisney",
+                    type: "image"
+                },
+                {
+                    name: "Brer Bear",
+                    author: "91JLovesDisney",
+                    type: "video"
+                },
+                {
+                    name: "Test3",
+                    author: "91JLovesDisney",
+                    type: "image"
+                },
+                {
+                    name: "Laughing Place",
+                    author: "91JLovesDisney",
+                    type: "audio"
+                },
+                {
+                    name: "HDYD Instrumental",
+                    author: "91JLovesDisney",
+                    type: "audio"
+                },
+                {
+                    name: "Brer Frog",
+                    author: "91JLovesDisney",
+                    type: "image"
+                },
+            ]));
+     });
+ }
+
 function refreshResults () {
     var PHPParams = "";
     for (i in filters) {
@@ -14,7 +51,8 @@ function refreshResults () {
     }
 
     //Fetch new results
-    fetch("/api/search" + PHPParams).then(response => response.json()).then((data) => {
+    // fetch("/api/search" + PHPParams).then(response => response.json()).then((data) => {
+    fetch("/api/search" + PHPParams).then(response => JSON.parse(response)).then((data) => {
         if (data.length === 0) {
             var noResults = document.createElement("p");
             noResults.className = "noResults";
@@ -22,14 +60,31 @@ function refreshResults () {
             document.querySelector(".resultsContainer").appendChild(noResults);
         } else {
             for (i in data) {
+                var currentItemData = data[i];
+
                 var resultElement = document.createElement("div");
                 resultElement.className = "result";
 
+                var headerContainer = document.createElement("div");
+                headerContainer.classList.add("headerContainer");
+
                 var name = document.createElement("h3");
                 name.classList.add("name");
-                name.textContent = data[i].name;
+                name.textContent = currentItemData.name;
 
-                resultElement.appendChild(name);
+                var type = document.createElement("p");
+                type.classList.add("type");
+                type.textContent = currentItemData.type;
+
+                var author = document.createElement("p");
+                author.classList.add("author");
+                author.textContent = currentItemData.author;
+
+                headerContainer.appendChild(name);
+                headerContainer.appendChild(type);
+
+                resultElement.appendChild(headerContainer);
+                resultElement.appendChild(author);
                 document.querySelector(".resultsContainer").appendChild(resultElement);
             }
         }
