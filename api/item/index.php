b419d76b1bf095887e80dab8a35e9111a26ecb38
em-3 <69275008+em-3@users.noreply.github.com>
2021-11-26 17:17:36 -0700

    Created the resource endpoint for the API
    Note: As of this commit the endpoint only supports retrieval.

---
+++ api/item/index.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . "../../../scripts/init.php";
+use SplmlFoundation\SplashMountainLegacyBackend\Resource;
+
+//Get the item id from the url
+$url = $_SERVER['REQUEST_URI'];
+$tokens = explode("/", substr($url, strlen("/api/item/")));
+$id = $tokens[0];
+
+if($_SERVER["REQUEST_METHOD"] == "GET") {
+    $resource = new Resource($id, $resource_path=__DIR__ . "/resources/");
+    $data = $resource->get_data();
+    $mime_type = $resource->get_mime_type();
+
+    header("Content-Type: " . $mime_type);
+    echo $data;
+}else if($_SERVER["REQUEST_METHOD"] == "POST") {
+    //For now updating the resource is not supported so send the appropriate response code
+    http_response_code(501);
+}
+
+?>
