46c6a83248a20ffc320213d6602b2db71f5167a0
cadehunter <60188691+cadehunter@users.noreply.github.com>
2021-12-06 20:24:23 -0700

    Added Filters to the Database Browser

--- js/databaseBrowser.js
+++ js/databaseBrowser.js
@@ -1,47 +1,81 @@
-var filters = [];
+var filters = [
+    {
+        id: "park",
+        label: "Park",
+        values: [
+            "All",
+            "WDW",
+            "DL",
+            "TDL"
+        ]
+    },
+    {
+        id: "type",
+        label: "Type",
+        values: [
+            "All",
+            "image",
+            "video",
+            "audio",
+            "text"
+        ]
+    },
+    {
+        id: "format",
+        label: "Format",
+        values: [
+            "All",
+            "image/png",
+            "image/jpeg",
+            "video/mp4",
+            "audio/mp3",
+            "audio/wav",
+            "text/txt"
+        ]
+    }
+];
 
-// function fetch() {
-//      return new Promise(function (resolve, reject) {
-//          resolve(JSON.stringify([
-//                 {
-//                     name: "Splash Mountain at Night",
-//                     author: "91JLovesDisney",
-//                     type: "image"
-//                 },
-//                 {
-//                     name: "Brer Bear",
-//                     author: "91JLovesDisney",
-//                     type: "video"
-//                 },
-//                 {
-//                     name: "Test3",
-//                     author: "91JLovesDisney",
-//                     type: "image"
-//                 },
-//                 {
-//                     name: "Laughing Place",
-//                     author: "91JLovesDisney",
-//                     type: "audio"
-//                 },
-//                 {
-//                     name: "HDYD Instrumental",
-//                     author: "91JLovesDisney",
-//                     type: "audio"
-//                 },
-//                 {
-//                     name: "Brer Frog",
-//                     author: "91JLovesDisney",
-//                     type: "image"
-//                 },
-//             ]));
-//      });
-//  }
+//Loop through each filter and create an element for for it.
+for (var i = 0; i < filters.length; i++) {
+    var currentFilter = filters[i];
+
+    var filterElement = document.createElement("div");
+    filterElement.classList.add("filter");
+    filterElement.classList.add(currentFilter.id);
+
+    var filterName = document.createElement("p");
+    filterName.classList.add("name");
+    filterName.textContent = currentFilter.label + ":";
+
+    var filterSelect = document.createElement("select");
+    filterSelect.setAttribute("name", currentFilter.id);
+    filterSelect.addEventListener("change", function () {
+        refreshResults();
+    });
+
+    for (var j = 0; j < currentFilter.values.length; j++) {
+        var filterSelectOption = document.createElement("option");
+        filterSelectOption.setAttribute("value", currentFilter.values[j]);
+        filterSelectOption.textContent = currentFilter.values[j];
+        if (j === 0) {
+            filterSelectOption.setAttribute("selected", true);
+        }
+        filterSelect.appendChild(filterSelectOption);
+    }
+
+    filterElement.appendChild(filterName);
+    filterElement.appendChild(filterSelect);
+    document.querySelector(".filterBar").appendChild(filterElement);
+}
 
 function refreshResults () {
     var PHPParams = "";
-    for (i in filters) {
-        if (filters[i].value != "") {
-            PHPParams += ((i === 0) ? "?" : "&") + filters[i].name + "=" + filters[i].value;
+    for (var i = 0; i < filters.length; i++) {
+        //Get the selected option for this filter's select element.
+        var filterElement = document.querySelector("select[name='" + filters[i].id + "']");
+        var filterValue = filterElement.options[filterElement.selectedIndex].value;
+        if (filterValue != "" && filterValue != "All") {
+            PHPParams += ((i === 0) ? "?" : "&") + filters[i].id + "=" + filterValue;
         }
     }
 
@@ -51,8 +85,7 @@ function refreshResults () {
     }
 
     //Fetch new results
-    fetch("/api/search" + PHPParams).then(response => response.json()).then((data) => {
-    // fetch("/api/search" + PHPParams).then(response => JSON.parse(response)).then((data) => {
+    fetch("https://splashmountainlegacy.com/api/search/" + PHPParams).then(response => response.json()).then((data) => {
         if (data.length === 0) {
             var noResults = document.createElement("p");
             noResults.className = "noResults";
