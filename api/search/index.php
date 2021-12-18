fa24d6bb1e217c7a5f63d36ecd85e4ae9d79a0d5
em-3 <69275008+em-3@users.noreply.github.com>
2021-12-18 09:50:05 -0700

    Added the ability to search by item ID in the search endpoint

--- api/search/index.php
+++ api/search/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . "/../../scripts/init.php";
 
 $available_params = ["type", "format", "park"];
 $params = array();
+$id_only = false;
 $results;
 $stmt = "SELECT * FROM `item_index` WHERE";
 
@@ -13,42 +14,54 @@ if(isset($_GET["query"])) {
     //Add the query to the list of parameters
     $params["query"] = $query;
 
-    $stmt .= " MATCH ( name, author, description ) AGAINST ( :query IN BOOLEAN MODE )";
-}
-
-foreach($available_params as $param) {
-    //Check if the parameter was set
-    if(isset($_GET[$param])) {
-        //If there is more than one parameter, add an AND
-        if(count($params) > 0) {
-            $stmt .= " AND";
-        }
-
-        //Set the parameter's value
-        $params[$param] = $_GET[$param];
-
-        //Add the parameter to the statement
-        $stmt .= " `$param` = :$param";
+    if(preg_match("/[A-Za-z0-9\/]{11}/", $query)) {
+        //If the query is a valid item ID, return only that item
+        $id_only = true;
+        $stmt .= " `id` = :query";
+    }else {
+        //Search for the query in name, author, and description fields
+        $stmt .= " MATCH ( name, author, description ) AGAINST ( :query IN BOOLEAN MODE )";
     }
 }
 
-if(isset($_GET["max"])) {
-    $max = intval($_GET["max"]);
+//If id only mode is enabled, then ignore all other parameters
+if(!$id_only) {
 
-    if(isset($_GET["min"])) {
-        //If there's a minimum value specified, then add it to the parameters and calculate the difference between the min and max values
+    foreach($available_params as $param) {
+        //Check if the parameter was set
+        if(isset($_GET[$param])) {
+            //If there is more than one parameter, add an AND
+            if(count($params) > 0) {
+                $stmt .= " AND";
+            }
+
+            //Set the parameter's value
+            $params[$param] = $_GET[$param];
+
+            //Add the parameter to the statement
+            $stmt .= " `$param` = :$param";
+        }
+    }
+
+    if(isset($_GET["max"])) {
+        $max = intval($_GET["max"]);
+
+        if(isset($_GET["min"])) {
+            //If there's a minimum value specified, then add it to the parameters and calculate the difference between the min and max values
         $min = intval($_GET["min"]);
 
-        $params["min"] = $min - 1;
-        $params["max"] = $max - $min;
-    }else {
-        //Otherwise just add zero and the max value
-        $params["min"] = 0;
-        $params["max"] = $max;
+            $params["min"] = $min - 1;
+            $params["max"] = $max - $min;
+        }else {
+            //Otherwise just add zero and the max value
+            $params["min"] = 0;
+            $params["max"] = $max;
+        }
+
+        //Update the statement to include the min and max
+        $stmt .= " LIMIT ?, ?";
     }
 
-    //Update the statement to include the min and max
-    $stmt .= " LIMIT ?, ?";
 }
 
 //Prepare the statement
