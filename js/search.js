a763997fc24c8c8208bebb408936c410626a577b
cadehunter <60188691+cadehunter@users.noreply.github.com>
2021-12-26 13:20:38 -0700

    Added front-end search
    All bugs fixed*
    
    GAHHHHHH
    
    mODAJFHIOSNIFNHIOSKLHFKFJSAOIFJOWJFOIEJOIAFOIHDIWFIEAW
    
    COMMITS COMMITS COMMITS
    
    Commit number 79
    
    Avengers: Infinity Commit
    
    And you thought I was done
    
    Aren't bugs fun?

--- js/search.js
+++ js/search.js
@@ -1,74 +1,4 @@
-function updateSearchResults() {
-    var query = document.querySelector(".searchField").value;
-    
-    //Wait two seconds before updating the search results
-    setTimeout(function () {
-        //Fetch new results
-        fetch("/api/search/?q=" + query).then(response => response.json()).then((results) => {
-            if (results.length === 0) {
-                var noResultsElement = document.createElement("p");
-                noResultsElement.className = "message";
-                noResultsElement.textContent = "No results found.";
-                document.querySelector(".resultsContainer").appendChild(noResultsElement);
-            } else {
-                for (var i = 0; i < results.length; i++) {
-                    var currentItemData = results[i];
-
-                    var resultElement = document.createElement("div");
-                    resultElement.className = "result";
-                    (function (id) {
-                        resultElement.onclick = function () {
-                            showItemDetails(id);
-                        }
-                    })(currentItemData.id)
-
-                    var headerContainer = document.createElement("div");
-                    headerContainer.classList.add("headerContainer");
-                    var subheaderContainer = document.createElement("div");
-                    subheaderContainer.classList.add("subheaderContainer");
-
-                    var name = document.createElement("h3");
-                    name.classList.add("name");
-                    name.textContent = currentItemData.name;
-
-                    var type = document.createElement("p");
-                    type.classList.add("type");
-                    type.textContent = currentItemData.type;
-
-                    var park = document.createElement("p");
-                    park.classList.add("park");
-                    park.textContent = currentItemData.park;
-
-                    var author = document.createElement("p");
-                    author.classList.add("author");
-                    author.textContent = currentItemData.author;
-
-                    headerContainer.appendChild(name);
-                    headerContainer.appendChild(type);
-
-                    subheaderContainer.appendChild(park);
-                    subheaderContainer.appendChild(author);
-
-                    resultElement.appendChild(headerContainer);
-                    resultElement.appendChild(subheaderContainer);
-                    document.querySelector(".resultsContainer").appendChild(resultElement);
-                }
-            }
-        }, (error) => {
-            var errorMessageElement = document.createElement("p");
-            errorMessageElement.className = "message";
-            errorMessageElement.textContent = "Something went wrong.";
-            document.querySelector(".resultsContainer").appendChild(errorMessageElement);
-        }
-    });
-    
-}
-
-function search(query) {
-    
-    
-    
-}
+var timeoutID = null;
 
 var searchBar = {
     onfocus: function () {
@@ -76,5 +6,94 @@ var searchBar = {
     },
     onblur: function () {
         document.querySelector("header").classList.remove("searchActive");
+    },
+
+    showLoadingContainer: function () {
+        document.querySelector(".searchResultsContainer .loadingContainer").classList.remove("hidden");
+        document.querySelector(".searchResultsContainer .resultsContainer").classList.add("hidden");
+    },
+    showResultsContainer: function () {
+        document.querySelector(".searchResultsContainer .loadingContainer").classList.add("hidden");
+        document.querySelector(".searchResultsContainer .resultsContainer").classList.remove("hidden");
+    },
+    updateSearchResults: function () {
+        var query = document.querySelector(".searchField").value;
+
+        if (query.length === 0) {
+            this.clearSearchResults();
+            this.showResultsContainer();
+            return;
+        }
+
+        if (timeoutID) {
+            clearTimeout(timeoutID);
+        }
+
+        this.showLoadingContainer();
+        
+        //Wait two seconds before updating the search results
+        timeoutID = setTimeout(function () {
+            //Fetch new results
+            fetch("/api/search/?query=" + query).then(response => response.json()).then((results) => {
+                searchBar.clearSearchResults();
+
+                if (results.length === 0) {
+                    var noResultsElement = document.createElement("p");
+                    noResultsElement.className = "message";
+                    noResultsElement.textContent = "No results found.";
+                    document.querySelector(".searchResultsContainer .resultsContainer").appendChild(noResultsElement);
+                } else {
+                    for (var i = 0; i < results.length; i++) {
+                        var currentItemData = results[i];
+
+                        var resultElement = document.createElement("div");
+                        resultElement.className = "result";
+                        (function (id) {
+                            resultElement.onmousedown = function () {
+                                showItemDetails(id);
+                            }
+                        })(currentItemData.id)
+
+                        var nameElement = document.createElement("h3");
+                        nameElement.className = "name";
+                        nameElement.textContent = currentItemData.name;
+
+                        var infoContainerElement = document.createElement("div");
+                        infoContainerElement.className = "infoContainer";
+
+                        var parkElement = document.createElement("p");
+                        parkElement.className = "park";
+                        parkElement.textContent = currentItemData.park;
+                        infoContainerElement.appendChild(parkElement);
+
+                        var typeElement = document.createElement("p");
+                        typeElement.className = "type";
+                        typeElement.textContent = currentItemData.type;
+                        infoContainerElement.appendChild(typeElement);
+
+                        var authorElement = document.createElement("p");
+                        authorElement.className = "author";
+                        authorElement.textContent = currentItemData.author;
+                        infoContainerElement.appendChild(authorElement);
+
+                        resultElement.appendChild(nameElement);
+                        resultElement.appendChild(infoContainerElement);
+                        document.querySelector(".searchResultsContainer .resultsContainer").appendChild(resultElement);
+                    }
+                    searchBar.showResultsContainer();
+                }
+            }, (error) => {
+                var errorMessageElement = document.createElement("p");
+                errorMessageElement.className = "message";
+                errorMessageElement.textContent = "Something went wrong.";
+                document.querySelector(".searchResultsContainer .resultsContainer").appendChild(errorMessageElement);
+                searchBar.showResultsContainer();
+            }
+        )}, 2000);
+    },
+    clearSearchResults: function () {
+        while (document.querySelector(".searchResultsContainer .resultsContainer").firstChild) {
+            document.querySelector(".searchResultsContainer .resultsContainer").removeChild(document.querySelector(".searchResultsContainer .resultsContainer").firstChild);
+        }
     }
 };
