f36ace7584b4f7387c1c99547894e43fc029d9dc
em-3 <69275008+em-3@users.noreply.github.com>
2022-01-02 09:40:01 -0700

    Created upload script for admin page

---
+++ admin/upload.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once __DIR__ . "/scripts/init_admin.php";
+require_once __DIR__ . "/scripts/item_utils.php";
+
+if(!check_authentication()) {
+    http_response_code(401);
+    header("Content-Type: application/json");
+    die(json_encode(["status" => "error", "error" => "Not authenticated"]));
+}
+
+if($_SERVER["REQUEST_METHOD"] !== "POST") {
+    header("Content-Type: application/json");
+    die(json_encode(["status" => "error", "error" => "Invalid request"]));
+}
+
+$resource_dir = __DIR__ . "/../resources";
+
+try {
+    $id = upload_item($_POST, $database, $_FILES["image"], $resource_dir);
+
+    header("Content-Type: application/json");
+    exit(json_encode(["status" => "success", "id" => $id]));
+}catch(Exception $e) {
+    header("Content-Type: application/json");
+    die(json_encode(["status" => "error", "error" => $e->getMessage()]));
+}
+
+?>
