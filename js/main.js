916e40f3f8b6fb6d07b6fef830724e44ff726932
cadehunter <60188691+cadehunter@users.noreply.github.com>
2022-06-24 15:41:56 -0600

    Fixed Malfunctioning Close Window Button in Item Viewer

--- js/main.js
+++ js/main.js
@@ -1,39 +1,40 @@
 function showItemDetails(id) {
-    document.querySelector(".itemDetailsContainer iframe").src = "/item?id=" + id;
-    document.querySelector(".itemDetailsContainer").classList.remove("hidden");
-    document.body.classList.add("noScroll");
+  document.querySelector(".itemDetailsContainer iframe").src =
+    "/item?id=" + id + "&embedded=true";
+  document.querySelector(".itemDetailsContainer").classList.remove("hidden");
+  document.body.classList.add("noScroll");
 }
 
 function hideItemDetails() {
-    document.querySelector(".itemDetailsContainer").classList.add("hidden");
-    document.body.classList.remove("noScroll");
+  document.querySelector(".itemDetailsContainer").classList.add("hidden");
+  document.body.classList.remove("noScroll");
 }
 
 //Listen for iframe requests
-window.onmessage = function(e) {
-    if (e.data.indexOf("details") === 0) {
-        showItemDetails(e.data.substring(7));
-    } else if (e.data === "closeDetails") {
-        hideItemDetails();
-    }
+window.onmessage = function (e) {
+  if (e.data.indexOf("details") === 0) {
+    showItemDetails(e.data.substring(7));
+  } else if (e.data === "closeDetails") {
+    hideItemDetails();
+  }
 };
 
 //Show admin console link if user has previously logged in
 if (localStorage.getItem("adminAccess") === "true") {
-    document.querySelector("header .links .admin").classList.remove("hidden");
+  document.querySelector("header .links .admin").classList.remove("hidden");
 }
 
 //Listen for the Konami code
 var konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 var konamiCodeIndex = 0;
 
-document.addEventListener("keydown", function(event) {
-    if (event.keyCode === konamiCode[konamiCodeIndex]) {
-        konamiCodeIndex++;
-    } else {
-        konamiCodeIndex = 0;
-    }
-    if (konamiCodeIndex === konamiCode.length) {
-        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-    }
+document.addEventListener("keydown", function (event) {
+  if (event.keyCode === konamiCode[konamiCodeIndex]) {
+    konamiCodeIndex++;
+  } else {
+    konamiCodeIndex = 0;
+  }
+  if (konamiCodeIndex === konamiCode.length) {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  }
 });
