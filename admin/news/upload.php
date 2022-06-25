20d30355d68ad41aafda4b51bdad872babd05e95
em-3 <69275008+em-3@users.noreply.github.com>
2022-06-24 18:13:56 -0600

    Added some documentation to the article upload endpoint

--- admin/news/upload.php
+++ admin/news/upload.php
@@ -15,38 +15,47 @@ if($_SERVER["REQUEST_METHOD"] !== "POST") {
 }
 
 try {
+    //Start a transaction
     $database->beginTransaction();
 
+    //If there is an ID, we are updating an existing article.
     if(isset($_POST["id"])) {
         $allowed_fields = ["title", "subtitle", "author", "content", "thumbnail"];
 
-        $database_entry = new DatabaseEntry($database, "news_articles", $allowed_fields, array(), $_POST["id"]);
+        //Update the existing article
+        $database_entry = new DatabaseEntry($database, "news_articles", array(), $allowed_fields, $_POST["id"]);
 
         $database_entry->loadData($_POST);
 
         $id = $database_entry->saveEntry();
 
+        //Commit the transaction
         $database->commit();
 
+        //Output the ID
         header("Content-Type: application/json");
         exit(json_encode(["status" => "success", "id" => $id]));
     }else {
         $required_fields = ["title", "subtitle", "author", "content", "thumbnail"];
         $optional_fields = ["publication_date"];
 
+        //Create a database entry for the article
         $database_entry = new DatabaseEntry($database, "news_articles", $required_fields, $optional_fields);
 
         $database_entry->loadData($_POST);
 
         $id = $database_entry->saveEntry();
 
+        //Commit the transaction 
         $database->commit();
 
+        //Output the ID
         header("Content-Type: application/json");
         exit(json_encode(["status" => "success", "id" => $id]));
     }
 }catch(Exception $e) {
     $database->rollBack();
+
     header("Content-Type: application/json");
     die(json_encode(["status" => "error", "error" => $e->getMessage()]));
 }
