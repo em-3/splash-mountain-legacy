236ad97061eff423e352950ee375f5fc8caac5a1
em-3 <69275008+em-3@users.noreply.github.com>
2022-06-24 18:30:01 -0600

    Moved news article updating functionality to a separate endpoint

--- admin/news/update.php
+++ admin/news/update.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . "/../scripts/init_admin.php";
 use SplmlFoundation\SplashMountainLegacyBackend\DatabaseEntry;
+use SplmlFoundation\SplashMountainLegacyBackend\ResourceManager;
 
 if(!check_authentication()) {
     http_response_code(401);
@@ -14,21 +15,26 @@ if($_SERVER["REQUEST_METHOD"] !== "POST") {
     die(json_encode(["status" => "error", "error" => "Invalid request"]));
 }
 
+$resource_dir = __DIR__ . "/../../resources";
+
 try {
     //Start a transaction
     $database->beginTransaction();
 
-    $required_fields = ["title", "subtitle", "author", "content", "thumbnail"];
-    $optional_fields = ["publication_date"];
+    if(!isset($_POST["id"])) {
+        throw new Exception("ID field required to update article!");
+    }
 
-    //Create a database entry for the article
-    $database_entry = new DatabaseEntry($database, "news_articles", $required_fields, $optional_fields);
+    $allowed_fields = ["title", "subtitle", "author", "content", "thumbnail"];
+
+    //Update the existing article
+    $database_entry = new DatabaseEntry($database, "news_articles", array(), $allowed_fields, $_POST["id"]);
 
     $database_entry->loadData($_POST);
 
     $id = $database_entry->saveEntry();
 
-    //Commit the transaction 
+    //Commit the transaction
     $database->commit();
 
     //Output the ID
@@ -40,5 +46,3 @@ try {
     header("Content-Type: application/json");
     die(json_encode(["status" => "error", "error" => $e->getMessage()]));
 }
-
-?>
