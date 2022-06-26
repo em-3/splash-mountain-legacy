63dc53f8b1c32cbd52cf45f537e90ffafce06153
em-3 <69275008+em-3@users.noreply.github.com>
2022-06-26 12:47:26 -0600

    Rewrote item upload endpoint to use the new OO resource system

--- admin/item/upload.php
+++ admin/item/upload.php
@@ -1,5 +1,8 @@
 <?php
 
+use SplmlFoundation\SplashMountainLegacyBackend\ItemEntry;
+use SplmlFoundation\SplashMountainLegacyBackend\Resource;
+
 require_once __DIR__ . "/../scripts/init_admin.php";
 require_once __DIR__ . "/../scripts/item_utils.php";
 
@@ -17,24 +20,47 @@ if($_SERVER["REQUEST_METHOD"] !== "POST") {
 $resource_dir = __DIR__ . "/../../resources";
 
 try {
-    $image_data = null;
+    $database->beginTransaction();
 
+    //Require that an ID is given
+    if(!isset($_POST["id"])) {
+        throw new Exception("No ID provided");
+    }
+
+    $id = $_POST["id"];
+
+    //Create an empty item
+    $item = new ItemEntry($database, "item_index", $id);
+    //Create an empty resource
+    $resource = new Resource($database, "resource_associations", $resource_dir);
+
+    //Check if the image field was provided and if it is a file
     if(isset($_FILES["image"])) {
-        $image_data = $_FILES["image"];
+        //If it is a file, upload it, generate an ID from it's hash, and store that ID in an affiliated records table.
+        $resource->useUploadedImage($_FILES["image"]);
+        $resource->associateID($id);
+
+        $item->setDataField("image", $resource->getID());
+    }else if(isset($_POST["image"])) {
+        //If it is not a file, add it to the item's data
+        $item->setDataField("image", $_POST["image"]);
     }
 
-    if(isset($_POST["id"])) {
-        $id = modify_item($_POST, $database);
+    //Loop through the provided POST parameters, and ensure that POST contains all the required item data
+    $item->putRequiredData($_POST);
+    //Add the required data to the item's data, as well as the optional data.
+    $item->putOptionalData($_POST);
+    //Store the item's data in the item's table.
+    $item->commit();
+    $resource->commitWithCleanup();
 
-        header("Content-Type: application/json");
-        exit(json_encode(["status" => "success", "id" => $id]));
-    }else {
-        $id = upload_item($_POST, $database, $image_data, $resource_dir);
+    $database->commit();
 
-        header("Content-Type: application/json");
-        exit(json_encode(["status" => "success", "id" => $id]));
-    }
+    header("Content-Type: application/json");
+    exit(json_encode(["status" => "success", "id" => $item->getID()]));
 }catch(Exception $e) {
+    $database->rollBack();
+
     header("Content-Type: application/json");
     die(json_encode(["status" => "error", "error" => $e->getMessage()]));
 }
