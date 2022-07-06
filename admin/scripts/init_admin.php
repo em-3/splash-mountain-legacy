ae745ece0a9a9c8b1136dea1a12fd6ef09f312d7
em-3 <69275008+em-3@users.noreply.github.com>
2022-07-06 13:19:14 -0600

    Moved generate_id() to init_admin.php

--- admin/scripts/init_admin.php
+++ admin/scripts/init_admin.php
@@ -13,4 +13,37 @@ function check_authentication() {
     return isset($_SESSION["id"]) && $_SESSION["id"] != "";
 }
 
+/**
+ * Generates a random unique Base38 URL ID.
+ * @param PDO $database The database to check the generated ID against
+ * @param string $table_name The name of the table to check the generated ID against
+ * @return string The random Base38 URL ID
+ * @throws Exception If the ID could not be generated
+ */
+function generate_id($database, $table_name) {
+    $index = "abcdefghijklmnopqrstuvwxyz0123456789-_";
+
+    $stmt = $database->prepare("SELECT id FROM `$table_name` WHERE `id` = ?");
+
+    for($i = 0; $i < 100; $i++) {
+
+        //Generate an id
+        $id = "";
+        for($i = 0; $i < 11; $i++) {
+            $id .= $index[mt_rand(0, 37)];
+        }
+        
+        //Check if the ID is unique
+        $stmt->bindValue(1, $id);
+        $stmt->execute();
+        
+        //If the result set is empty, then the ID is unique
+        if($stmt->rowCount() == 0) {
+            return $id;
+        }
+    }
+
+    throw new \Exception("Failed to generate unique ID");
+}
+
 ?>
