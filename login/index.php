fd8de82118f594d57d68f9e36e384a818697c5f1
cadehunter <60188691+cadehunter@users.noreply.github.com>
2022-07-11 16:16:28 -0600

    Part 2 of Redesigned Login Page

--- login/index.php
+++ login/index.php
@@ -68,44 +68,45 @@ if(isset($_GET["code"])) {
 
 <!DOCTYPE HTML>
 <html>
-    
-    <head>
-        
-        <meta charset="utf-8">
-        <title>Login - Splash Mountain Legacy</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover, user-scalable=no">
-        
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="/css/global.css">
-        <link rel="stylesheet" href="/css/main.css">
-        <link rel="stylesheet" href="styles.css">
-        
-        <meta name="description" content="Splash Mountain images, videos, audio, and more.">
-        <meta name="keywords" content="Splash Mountain, Splash Mountain Legacy, SaveSplashMountain, Critter Country, Frontierland, Disneyland, Tokyo Disneyland, Magic Kingdom, Walt Disney World">
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#2f8ea9">
-        <meta name="msapplication-TileColor" content="#2d89ef">
-        
-        <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)">
-        <meta name="theme-color" content="#222222" media="(prefers-color-scheme: dark)">
-        
-    </head>
-    
-    <body ontouchstart class>
-        
-        <header>
-            <picture class="logo" onclick="window.location.href = '/'">
-                <source srcset="/images/logo-white.png" media="(prefers-color-scheme: dark)">
-                <img src="/images/logo-black.png">
-            </picture>
-        
-            <!-- <div class="linksContainer">
+<head>
+
+    <meta charset="utf-8">
+    <title>Login - Splash Mountain Legacy</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover, user-scalable=no">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/global.css">
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <meta name="description" content="Splash Mountain images, videos, audio, and more.">
+    <meta name="keywords"
+        content="Splash Mountain, Splash Mountain Legacy, SaveSplashMountain, Critter Country, Frontierland, Disneyland, Tokyo Disneyland, Magic Kingdom, Walt Disney World">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#2f8ea9">
+    <meta name="msapplication-TileColor" content="#2d89ef">
+
+    <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)">
+    <meta name="theme-color" content="#222222" media="(prefers-color-scheme: dark)">
+
+</head>
+
+<body ontouchstart class>
+
+    <header>
+        <picture class="logo" onclick="window.location.href = '/'">
+            <source srcset="/images/logo-white.png" media="(prefers-color-scheme: dark)">
+            <img src="/images/logo-black.png">
+        </picture>
+
+        <!-- <div class="linksContainer">
                 <div class="links">
                     <a href="/">Home</a>
                     <a href="/database">Database</a>
@@ -130,110 +131,110 @@ if(isset($_GET["code"])) {
                     </picture>
                 </div>
             </div> -->
-        </header>
+    </header>
 
-        <div class="searchResultsContainer">
-            <div class="loadingContainer hidden">
-                <div class="loadingAnimationEllipsis">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-            </div>
-            <div class="resultsContainer"></div>
-            <div class="errorMessageContainer">
-                <h2 class="title"></h2>
-                <p class="subtitle"></p>
+    <div class="searchResultsContainer">
+        <div class="loadingContainer hidden">
+            <div class="loadingAnimationEllipsis">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
         </div>
+        <div class="resultsContainer"></div>
+        <div class="errorMessageContainer">
+            <h2 class="title"></h2>
+            <p class="subtitle"></p>
+        </div>
+    </div>
 
-        <main>
-            <section>
-                <?php if(!isset($user)) { ?>
-                    <!-- Authentication not started or didn't succeed -->
-                    <div class="loginPromptContainer">
-                        <h1>Login</h1>
-                        <?php if($error != "") { ?>
-                                <p class="errorMessage"><?php echo $error; ?></p>
-                        <?php } ?>
-                    </div>
-                    <div>
-                        <button class="loginButton" onclick="window.location.href='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>?login=discord'">Login with Discord</a>
-                    </div>
-                <?php }else { ?>
-                    <!-- Authentication succeeded -->
-                    <div class="welcomeContainer">
-                        <img class="profilePicture" src="https://cdn.discordapp.com/avatars/<?php echo $user->getID() . "/" . $user->getAvatarHash(); ?>">
-                        <h1 class="name"><?php echo $user->getUsername() . "#" . $user->getDiscriminator(); ?></h1>
-                        <h2 class="authLevel">
-                            <?php
-                                if(!isset($_SESSION["clearance"])) {
-                                    echo "Unauthorized";
-                                } else if($_SESSION["clearance"] == 0) {
-                                    echo "Admin";
-                                } else {
-                                    echo "Launch Team";
-                                }
-                            ?>
-                        </h2>
-                        <?php if(check_authentication()) { ?>
-                            <!-- User is allowed access -->
-                            <a href="/" class="continueButton">Continue</a>
-                        <?php }else { ?>
-                            <!-- User is not allowed access -->
-                            <p class="authLevelWarning">Sorry, you don't have access to the site yet. Don't worry, Splash Mountain Legacy will be available to everyone soon!</p>
-                        <?php } ?>
-                    </div>
+    <main>
+        <?php if(!isset($user)) { ?>
+        <!-- Authentication not started or didn't succeed -->
+        <section class="loginPromptContainer">
+            <div class="titleContainer">
+                <h1>Login</h1>
+                <?php if($error != "") { ?>
+                <p class="errorMessage">
+                    <?php echo $error; ?>
+                </p>
                 <?php } ?>
-            </section>
-        </main>
+            </div>
+            <button class="loginButton" onclick="window.location.href='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>?login=discord'">Login with Discord</a>
+        </section>
+        <?php }else { ?>
+        <!-- Authentication succeeded -->
+        <section class="welcomeContainer">
+            <img class="profilePicture" src="https://cdn.discordapp.com/avatars/<?php echo $user->getID() . "/" . $user->getAvatarHash(); ?>">
+            <h1 class="name"><?php echo $user->getUsername() . "#" . $user->getDiscriminator(); ?></h1>
+            <h3 class="authLevel">
+                <?php
+                    if(!isset($_SESSION["clearance"])) {
+                        echo "Unauthorized";
+                    } else if($_SESSION["clearance"] == 0) {
+                        echo "Admin";
+                    } else {
+                        echo "Launch Team";
+                    }
+                ?>
+            </h3>
+            <?php if(check_authentication()) { ?>
+            <!-- User is allowed access -->
+            <button class="continueButton" onclick="window.location.href='/'">Continue</button>
+            <?php }else { ?>
+            <!-- User is not allowed access -->
+            <p class="authLevelWarning">Sorry, you don't have access to the site yet.</p>
+            <?php } ?>
+        </section>
+        <?php } ?>
+    </main>
 
-        <footer>
-            <div class="logo">
-                <img src="/images/logo-white.png" onclick="window.location.href = '/'">
-            </div>
-            <div class="credits">
-                <h3>Created By</h3>
-                <a href="https://www.youtube.com/channel/UCJau8EhvBih5QfGb3s2nVvA" target="_BLANK">91J Loves Disney</a>
-                <a>EM_3</a>
-                <a href="https://www.youtube.com/channel/UCnL5QGcUhQo1SLOuL23yG1A" target="_BLANK">MickeyWaffleCo.</a>
-            </div>
-            <div class="links">
-                <a class="link email" href="mailto:splashmountainlegacy@gmail.com" target="_BLANK">
-                    <picture class="regular">
-                        <source srcset="/images/footer/email-white.png" media="(prefers-color-scheme: dark)">
-                        <img src="/images/footer/email-black.png" alt="Email" width="22px" height="auto">
-                    </picture>
-                    <img src="/images/footer/email-white.png" alt="Email" width="22px" height="auto" class="hover">
-                </a>
-                <a class="link youtube" href="https://www.youtube.com/channel/UCmh3ksrwPB-NRcfMjGUbYFw" target="_BLANK">
-                    <picture class="regular">
-                        <source srcset="/images/footer/youtube-white.png" media="(prefers-color-scheme: dark)">
-                        <img src="/images/footer/youtube-black.png" alt="Youtube" width="22px" height="auto">
-                    </picture>
-                    <img src="/images/footer/youtube-white.png" alt="Youtube" width="22px" height="auto" class="hover">
-                </a>
-                <a class="link discord" href="https://discord.gg/GwThxWbQMG" target="_BLANK">
-                    <picture class="regular">
-                        <source srcset="/images/footer/discord-white.png" media="(prefers-color-scheme: dark)">
-                        <img src="/images/footer/discord-black.png" alt="Youtube" width="22px" height="auto">
-                    </picture>
-                    <img src="/images/footer/discord-white.png" alt="Discord" width="22px" height="auto" class="hover">
-                </a>
-            </div>
-        </footer>
-
-        <div class="itemDetailsContainer hidden">
-            <iframe src="" frameborder="0"></iframe>
+    <footer>
+        <div class="logo">
+            <img src="/images/logo-white.png" onclick="window.location.href = '/'">
         </div>
+        <div class="credits">
+            <h3>Created By</h3>
+            <a href="https://www.youtube.com/channel/UCJau8EhvBih5QfGb3s2nVvA" target="_BLANK">91J Loves Disney</a>
+            <a>EM_3</a>
+            <a href="https://www.youtube.com/channel/UCnL5QGcUhQo1SLOuL23yG1A" target="_BLANK">MickeyWaffleCo.</a>
+        </div>
+        <div class="links">
+            <a class="link email" href="mailto:splashmountainlegacy@gmail.com" target="_BLANK">
+                <picture class="regular">
+                    <source srcset="/images/footer/email-white.png" media="(prefers-color-scheme: dark)">
+                    <img src="/images/footer/email-black.png" alt="Email" width="22px" height="auto">
+                </picture>
+                <img src="/images/footer/email-white.png" alt="Email" width="22px" height="auto" class="hover">
+            </a>
+            <a class="link youtube" href="https://www.youtube.com/channel/UCmh3ksrwPB-NRcfMjGUbYFw" target="_BLANK">
+                <picture class="regular">
+                    <source srcset="/images/footer/youtube-white.png" media="(prefers-color-scheme: dark)">
+                    <img src="/images/footer/youtube-black.png" alt="Youtube" width="22px" height="auto">
+                </picture>
+                <img src="/images/footer/youtube-white.png" alt="Youtube" width="22px" height="auto" class="hover">
+            </a>
+            <a class="link discord" href="https://discord.gg/GwThxWbQMG" target="_BLANK">
+                <picture class="regular">
+                    <source srcset="/images/footer/discord-white.png" media="(prefers-color-scheme: dark)">
+                    <img src="/images/footer/discord-black.png" alt="Youtube" width="22px" height="auto">
+                </picture>
+                <img src="/images/footer/discord-white.png" alt="Discord" width="22px" height="auto" class="hover">
+            </a>
+        </div>
+    </footer>
 
-        <div class="overlay"></div>
-        
-        <script src="/js/global.js"></script>
-        <script src="/js/main.js"></script>
-        <script src="/js/search.js"></script>
-        
-    </body>
-    
+    <div class="itemDetailsContainer hidden">
+        <iframe src="" frameborder="0"></iframe>
+    </div>
+
+    <div class="overlay"></div>
+
+    <script src="/js/global.js"></script>
+    <script src="/js/main.js"></script>
+    <script src="/js/search.js"></script>
+
+</body>
+
 </html>
