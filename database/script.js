ccd8e9ebe9154fd066fd12d1de8418ba9212721f
cadehunter <60188691+cadehunter@users.noreply.github.com>
2022-07-11 16:49:10 -0600

    Set Maximum of One Filter for Park and Type

--- database/script.js
+++ database/script.js
@@ -3,11 +3,13 @@ var filters = [
 		id: "park",
 		label: "Park",
 		values: ["WDW", "DL", "TDL"],
+		max: 1,
 	},
 	{
 		id: "type",
 		label: "Type",
 		values: ["image", "video", "audio", "text"],
+		max: 1,
 	},
 ];
 
@@ -40,9 +42,7 @@ for (var i = 0; i < filters.length; i++) {
 	filterName.textContent = currentFilter.label;
 
 	filterElement.appendChild(filterName);
-	document
-		.querySelector(".filterSelect .availableFilters")
-		.appendChild(filterElement);
+	document.querySelector(".filterSelect .availableFilters").appendChild(filterElement);
 }
 
 function addFilter(filterObject) {
@@ -74,6 +74,7 @@ function addFilter(filterObject) {
 	removeButton.classList.add("removeButton");
 	removeButton.textContent = "✕";
 	removeButton.addEventListener("click", function () {
+		document.querySelector(".availableFilters .filter." + filterObject.id).classList.remove("disabled");
 		filterElement.parentElement.removeChild(filterElement);
 		databaseBrowser.refreshResults();
 	});
@@ -82,6 +83,11 @@ function addFilter(filterObject) {
 	filterElement.appendChild(filterSelect);
 	filterElement.appendChild(removeButton);
 	document.querySelector(".filterBar .filters").appendChild(filterElement);
+
+	var filterCount = document.querySelectorAll(".filterBar .filter." + filterObject.id).length;
+	if (filterObject.max === filterCount) {
+		document.querySelector(".availableFilters .filter." + filterObject.id).classList.add("disabled");
+	}
 
 	databaseBrowser.refreshResults();
 }
@@ -99,15 +105,9 @@ var databaseBrowser = {
 				clearTimeout(this.timeoutID);
 			}
 
-			document
-				.querySelector(".databaseBrowser .loadingContainer")
-				.classList.remove("hidden");
-			document
-				.querySelector(".databaseBrowser .resultsContainer")
-				.classList.add("hidden");
-			document
-				.querySelector(".databaseBrowser .errorMessageContainer")
-				.classList.add("hidden");
+			document.querySelector(".databaseBrowser .loadingContainer").classList.remove("hidden");
+			document.querySelector(".databaseBrowser .resultsContainer").classList.add("hidden");
+			document.querySelector(".databaseBrowser .errorMessageContainer").classList.add("hidden");
 
 			//Wait a second before updating the search results
 			this.timeoutID = setTimeout(databaseBrowser.refreshResults, 1000);
@@ -126,17 +126,12 @@ var databaseBrowser = {
 			character = "&";
 		}
 
-		var activeFilters = document.querySelectorAll(
-			".filterBar .filters .filter"
-		);
+		var activeFilters = document.querySelectorAll(".filterBar .filters .filter");
 		for (var i = 0; i < activeFilters.length; i++) {
 			var filterID = activeFilters[i].classList[1];
 			//Get the selected option for this filter's select element.
-			var filterElement = document.querySelector(
-				"select[name='" + filterID + "']"
-			);
-			var filterValue =
-				filterElement.options[filterElement.selectedIndex].value;
+			var filterElement = document.querySelector("select[name='" + filterID + "']");
+			var filterValue = filterElement.options[filterElement.selectedIndex].value;
 			if (filterValue != "") {
 				PHPParams += character + filterID + "=" + filterValue;
 				character = "&";
@@ -145,30 +140,13 @@ var databaseBrowser = {
 
 		if (!preservePreviousResults) {
 			//Clear the current results from .resultsContainer
-			while (
-				document.querySelector(".databaseBrowser .resultsContainer")
-					.firstChild
-			) {
-				document
-					.querySelector(".databaseBrowser .resultsContainer")
-					.removeChild(
-						document.querySelector(
-							".databaseBrowser .resultsContainer"
-						).firstChild
-					);
+			while (document.querySelector(".databaseBrowser .resultsContainer").firstChild) {
+				document.querySelector(".databaseBrowser .resultsContainer").removeChild(document.querySelector(".databaseBrowser .resultsContainer").firstChild);
 			}
 		}
 
 		//Fetch new results
-		fetch(
-			"/api/search/" +
-				PHPParams +
-				character +
-				"min=" +
-				this.searchRange.min +
-				"&max=" +
-				this.searchRange.max
-		)
+		fetch("/api/search/" + PHPParams + character + "min=" + this.searchRange.min + "&max=" + this.searchRange.max)
 			.then((response) => response.json())
 			.then(
 				(data) => {
@@ -176,24 +154,14 @@ var databaseBrowser = {
 						var noResults = document.createElement("p");
 						noResults.className = "noResults";
 						noResults.textContent = "No results found.";
-						document
-							.querySelector(".databaseBrowser .resultsContainer")
-							.appendChild(noResults);
+						document.querySelector(".databaseBrowser .resultsContainer").appendChild(noResults);
 					} else if (preservePreviousResults && data.length === 0) {
-						var loadMoreButton = document.querySelector(
-							".databaseBrowser .loadMoreButton"
-						);
-						loadMoreButton.parentElement.removeChild(
-							loadMoreButton
-						);
+						var loadMoreButton = document.querySelector(".databaseBrowser .loadMoreButton");
+						loadMoreButton.parentElement.removeChild(loadMoreButton);
 					} else {
 						if (preservePreviousResults) {
-							var loadMoreButton = document.querySelector(
-								".databaseBrowser .loadMoreButton"
-							);
-							loadMoreButton.parentElement.removeChild(
-								loadMoreButton
-							);
+							var loadMoreButton = document.querySelector(".databaseBrowser .loadMoreButton");
+							loadMoreButton.parentElement.removeChild(loadMoreButton);
 						}
 
 						for (var i = 0; i < data.length; i++) {
@@ -211,54 +179,35 @@ var databaseBrowser = {
 								var pictureElement = null;
 								var imgElement = document.createElement("img");
 								imgElement.className = "image";
-								imgElement.src =
-									"/resources/" +
-									currentItemData.id +
-									"/thumbnail";
+								imgElement.src = "/resources/" + currentItemData.id + "/thumbnail";
 							} else if (currentItemData.type === "video") {
 								var pictureElement = null;
 								var imgElement = document.createElement("img");
 								imgElement.className = "image";
-								imgElement.src =
-									"https://img.youtube.com/vi/" +
-									currentItemData.video_id +
-									"/mqdefault.jpg";
+								imgElement.src = "https://img.youtube.com/vi/" + currentItemData.video_id + "/mqdefault.jpg";
 							} else {
-								var pictureElement =
-									document.createElement("picture");
+								var pictureElement = document.createElement("picture");
 								pictureElement.className = "image";
 
-								var sourceElement =
-									document.createElement("source");
-								sourceElement.srcset =
-									"/images/icons/types/" +
-									currentItemData.type +
-									"-white.png";
-								sourceElement.setAttribute(
-									"media",
-									"(prefers-color-scheme: dark)"
-								);
+								var sourceElement = document.createElement("source");
+								sourceElement.srcset = "/images/icons/types/" + currentItemData.type + "-white.png";
+								sourceElement.setAttribute("media", "(prefers-color-scheme: dark)");
 
 								var imgElement = document.createElement("img");
-								imgElement.src =
-									"/images/icons/types/" +
-									currentItemData.type +
-									"-black.png";
+								imgElement.src = "/images/icons/types/" + currentItemData.type + "-black.png";
 
 								pictureElement.appendChild(sourceElement);
 								pictureElement.appendChild(imgElement);
 							}
 
-							var rightSideContainer =
-								document.createElement("div");
+							var rightSideContainer = document.createElement("div");
 							rightSideContainer.className = "right";
 
 							var nameElement = document.createElement("h3");
 							nameElement.className = "name";
 							nameElement.textContent = currentItemData.name;
 
-							var infoContainerElement =
-								document.createElement("div");
+							var infoContainerElement = document.createElement("div");
 							infoContainerElement.className = "infoContainer";
 
 							var parkElement = document.createElement("p");
@@ -274,18 +223,12 @@ var databaseBrowser = {
 							if (currentItemData.author) {
 								var authorElement = document.createElement("p");
 								authorElement.className = "author";
-								authorElement.textContent =
-									currentItemData.author.replace(
-										/\[([^\][]+)]/g,
-										""
-									);
+								authorElement.textContent = currentItemData.author.replace(/\[([^\][]+)]/g, "");
 								infoContainerElement.appendChild(authorElement);
 							}
 
 							rightSideContainer.appendChild(nameElement);
-							rightSideContainer.appendChild(
-								infoContainerElement
-							);
+							rightSideContainer.appendChild(infoContainerElement);
 
 							if (pictureElement) {
 								resultElement.appendChild(pictureElement);
@@ -293,68 +236,33 @@ var databaseBrowser = {
 								resultElement.appendChild(imgElement);
 							}
 							resultElement.appendChild(rightSideContainer);
-							document
-								.querySelector(
-									".databaseBrowser .resultsContainer"
-								)
-								.appendChild(resultElement);
+							document.querySelector(".databaseBrowser .resultsContainer").appendChild(resultElement);
 						}
 
-						if (
-							data.length ===
-							this.searchRange.max - this.searchRange.min
-						) {
-							var loadMoreButton =
-								document.createElement("button");
+						if (data.length === this.searchRange.max - this.searchRange.min) {
+							var loadMoreButton = document.createElement("button");
 							loadMoreButton.className = "loadMoreButton";
 							loadMoreButton.textContent = "Load More";
-							loadMoreButton.addEventListener(
-								"click",
-								function () {
-									databaseBrowser.loadMoreResults();
-								}
-							);
-							document
-								.querySelector(
-									".databaseBrowser .resultsContainer"
-								)
-								.appendChild(loadMoreButton);
+							loadMoreButton.addEventListener("click", function () {
+								databaseBrowser.loadMoreResults();
+							});
+							document.querySelector(".databaseBrowser .resultsContainer").appendChild(loadMoreButton);
 						}
 					}
 
 					//Hide the loading screen and show the results container
-					document
-						.querySelector(".databaseBrowser .loadingContainer")
-						.classList.add("hidden");
-					document
-						.querySelector(".databaseBrowser .resultsContainer")
-						.classList.remove("hidden");
-					document
-						.querySelector(
-							".databaseBrowser .errorMessageContainer"
-						)
-						.classList.add("hidden");
+					document.querySelector(".databaseBrowser .loadingContainer").classList.add("hidden");
+					document.querySelector(".databaseBrowser .resultsContainer").classList.remove("hidden");
+					document.querySelector(".databaseBrowser .errorMessageContainer").classList.add("hidden");
 				},
 				(error) => {
-					document.querySelector(
-						".databaseBrowser .errorMessageContainer .title"
-					).textContent = "Something Went Wrong";
-					document.querySelector(
-						".databaseBrowser .errorMessageContainer .subtitle"
-					).textContent = "Failed to load database items.";
+					document.querySelector(".databaseBrowser .errorMessageContainer .title").textContent = "Something Went Wrong";
+					document.querySelector(".databaseBrowser .errorMessageContainer .subtitle").textContent = "Failed to load database items.";
 
 					//Hide the loading screen and show the error message container
-					document
-						.querySelector(".databaseBrowser .loadingContainer")
-						.classList.add("hidden");
-					document
-						.querySelector(".databaseBrowser .resultsContainer")
-						.classList.add("hidden");
-					document
-						.querySelector(
-							".databaseBrowser .errorMessageContainer"
-						)
-						.classList.remove("hidden");
+					document.querySelector(".databaseBrowser .loadingContainer").classList.add("hidden");
+					document.querySelector(".databaseBrowser .resultsContainer").classList.add("hidden");
+					document.querySelector(".databaseBrowser .errorMessageContainer").classList.remove("hidden");
 				}
 			);
 	},
