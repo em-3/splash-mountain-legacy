9cf9314d12234b9c2d953d00abacd37343804e90
cadehunter <60188691+cadehunter@users.noreply.github.com>
2022-07-15 21:30:09 -0600

    Re-enabled Countdown Page

--- countdown/script.js
+++ countdown/script.js
@@ -1,5 +1,5 @@
 var currentTimestamp;
-var launchTimestamp = new Date("2022-07-17T17:00:00Z");
+var launchTimestamp = new Date("2022-07-17T20:00:00Z");
 
 var container = document.querySelector("section");
 var title = document.querySelector(".title");
@@ -7,48 +7,58 @@ var subtitle = document.querySelector(".subtitle");
 var countdown = document.querySelector(".description");
 
 function updateCountdown() {
-    currentTimestamp = new Date();
-    var difference = launchTimestamp.getTime() - currentTimestamp.getTime();
+	currentTimestamp = new Date();
+	var difference = launchTimestamp.getTime() - currentTimestamp.getTime();
 
-    //If the launch timestamp is in the past, redirect to index.html.
-    if (difference <= 0) {
-        window.location.href = "/";
-    } else {
-        var countdownString = "";
+	//If the launch timestamp is in the past, redirect to index.html.
+	if (difference <= 0) {
+		window.location.href = "/";
+	} else {
+		var countdownString = "";
 
-        var days = Math.floor(difference / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+		var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+		var hours = Math.floor(
+			(difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+		);
+		var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-        if (days > 0) {
-            countdownString += days + ((days === 1) ? " day, " : " days, ");
-        }
-        if (hours > 0) {
-            countdownString += hours + ((hours === 1) ? " hour, " : " hours, ");
-        }
-        countdownString += minutes + ((minutes === 1) ? " minute, " : " minutes, ");
-        countdownString += seconds + ((seconds === 1) ? " second." : " seconds.");
+		if (days > 0) {
+			countdownString += days + (days === 1 ? " day, " : " days, ");
+		}
+		if (hours > 0) {
+			countdownString += hours + (hours === 1 ? " hour, " : " hours, ");
+		}
+		countdownString +=
+			minutes + (minutes === 1 ? " minute, " : " minutes, ");
+		countdownString += seconds + (seconds === 1 ? " second." : " seconds.");
 
-        countdown.textContent = countdownString;
-    }
+		countdown.textContent = countdownString;
+	}
 
-    if (difference < (60 * 1000)) { // within a minute
-        container.classList.add("finalCountdown")
-        title.textContent = Math.ceil(difference / 1000);
-    } else if (difference < (10 * 60 * 1000)) { // within 10 minutes
-        title.textContent = "We're almost there!";
-        subtitle.textContent = "Splash Mountain Legacy's official launch is just minutes away!";
-    } else if (difference < (60 * 60 * 1000)) { // within an hour
-        title.textContent = "It's almost time!";
-        subtitle.textContent = "Splash Mountain Legacy is about to open its doors to Splash fans worldwide.";
-    } else if (difference < (24 * 60 * 60 * 1000)) { // within a day
-        title.textContent = "It's not quite time yet!";
-        subtitle.textContent = "We're getting the site ready for launch!";
-    } else if (difference < (7 * 24 * 60 * 60 * 1000)) { // within a week
-        title.textContent = "You're early!";
-        subtitle.textContent = "Splash Mountain Legacy is coming soon!";
-    }
+	if (difference < 60 * 1000) {
+		// within a minute
+		container.classList.add("finalCountdown");
+		title.textContent = Math.ceil(difference / 1000);
+	} else if (difference < 10 * 60 * 1000) {
+		// within 10 minutes
+		title.textContent = "We're almost there!";
+		subtitle.textContent =
+			"Splash Mountain Legacy's official launch is just minutes away!";
+	} else if (difference < 60 * 60 * 1000) {
+		// within an hour
+		title.textContent = "It's almost time!";
+		subtitle.textContent =
+			"Splash Mountain Legacy is about to open its doors to Splash fans worldwide.";
+	} else if (difference < 24 * 60 * 60 * 1000) {
+		// within a day
+		title.textContent = "It's not quite time yet!";
+		subtitle.textContent = "We're getting the site ready for launch!";
+	} else if (difference < 7 * 24 * 60 * 60 * 1000) {
+		// within a week
+		title.textContent = "You're early!";
+		subtitle.textContent = "Splash Mountain Legacy is coming soon!";
+	}
 }
 
 updateCountdown();
