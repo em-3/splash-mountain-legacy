e8387a96c59f8cadf3218f722c0c59272d183046
em-3 <69275008+em-3@users.noreply.github.com>
2022-07-28 12:46:24 -0600

    Added audit logging to item and article entries

--- src/Entry/ArticleEntry.php
+++ src/Entry/ArticleEntry.php
@@ -2,7 +2,29 @@
 
 namespace SplmlFoundation\SplashMountainLegacyBackend\Entry;
 
+use SplmlFoundation\SplashMountainLegacyBackend\Logging\AuditLog;
+
 class ArticleEntry extends Entry {
+
+    /** @var AuditLog $audit_log */
+    private $audit_log;
+
+    public function __construct($database, $table_name, $id) {
+        parent::__construct($database, $table_name, $id);
+        $this->audit_log = new AuditLog($database, "audit_log", "article");
+    }
+
+    protected function handleEntryCreate($data) {
+        $this->audit_log->logAuditAction("create", $this->getID(), $_SESSION["id"], $data);
+    }
+
+    protected function handleEntryDelete() {
+        $this->audit_log->logAuditAction("delete", $this->getID(), $_SESSION["id"]);
+    }
+
+    protected function handleEntryUpdate($data) {
+        $this->audit_log->logAuditAction("modify", $this->getID(), $_SESSION["id"], $data);
+    }
 
     public static function getRequiredFields() {
         return ["title", "subtitle", "author", "content", "thumbnail"];
--- src/Entry/ItemEntry.php
+++ src/Entry/ItemEntry.php
@@ -2,7 +2,17 @@
 
 namespace SplmlFoundation\SplashMountainLegacyBackend\Entry;
 
+use SplmlFoundation\SplashMountainLegacyBackend\Logging\AuditLog;
+
 class ItemEntry extends Entry {
+
+    /** @var AuditLog $audit_log */
+    private $audit_log;
+
+    public function __construct($database, $table_name, $id) {
+        parent::__construct($database, $table_name, $id);
+        $this->audit_log = new AuditLog($database, "audit_log", "item");
+    }
 
     protected function handleDataPut(&$field, &$data) {
         //Check if the tags field is currently being put
@@ -10,6 +20,18 @@ class ItemEntry extends Entry {
             //Collapse the array into a string
             $data = implode(", ", $data);
         }
+    }
+
+    protected function handleEntryCreate($data) {
+        $this->audit_log->logAuditAction("create", $this->getID(), $_SESSION["id"], $data);
+    }
+
+    protected function handleEntryDelete() {
+        $this->audit_log->logAuditAction("delete", $this->getID(), $_SESSION["id"]);
+    }
+
+    protected function handleEntryUpdate($data) {
+        $this->audit_log->logAuditAction("modify", $this->getID(), $_SESSION["id"], $data);
     }
 
     public static function getRequiredFields() {
