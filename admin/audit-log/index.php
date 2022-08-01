c1da0ea2f473118a2d6c27158e99f75942ce8213
em-3 <69275008+em-3@users.noreply.github.com>
2022-08-01 12:36:25 -0600

    Added admin endpoint to access the audit log data

---
+++ admin/audit-log/index.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . "/../scripts/init_admin.php";
+require_once __DIR__ . "/../scripts/endpoint_utils.php";
+
+check_request("GET");
+
+$sql = "SELECT `id`, `timestamp`, `type`, `action`, `item_id`, `user_id`, `changes` FROM `audit_log`";
+
+try {
+    $stmt = $database->prepare($sql);
+    $stmt->execute();
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    //Output the ID
+    header("Content-Type: application/json");
+    exit(json_encode(["status" => "success", "audit_data" => $results]));
+}catch(Exception $e) {
+    header("Content-Type: application/json");
+    die(json_encode(["status" => "error", "error" => $e->getMessage()]));
+}
+
+?>
