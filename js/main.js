4b69eca16b6058b53a61551d7c4142b3c3a7cfba
cadehunter <60188691+cadehunter@users.noreply.github.com>
2023-01-19 18:42:21 -0700

    Fixed Issue with Header Code in Item Viewer

--- js/main.js
+++ js/main.js
@@ -42,87 +42,89 @@ window.addEventListener("message", function (e) {
 });
 
 //Highlight the proper link in the header for the current page
-var linkAssociations = {
-	"login": null,
-	"admin": "admin",
-	"admin/database": "admin",
-	"admin/news": "admin",
-	"admin/audit": "admin",
-	"database": "database",
-	"item": "database",
-	"news": "news",
-	"article": "news",
-	"about": "about",
-	"/": "home"
-};
-var pathname = window.location.pathname;
-for (var key in linkAssociations) {
-	if (pathname.indexOf(key) !== -1) {
-		if (linkAssociations[key] === null) {
+if (document.querySelector("header .links")) {
+	var linkAssociations = {
+		"login": null,
+		"admin": "admin",
+		"admin/database": "admin",
+		"admin/news": "admin",
+		"admin/audit": "admin",
+		"database": "database",
+		"item": "database",
+		"news": "news",
+		"article": "news",
+		"about": "about",
+		"/": "home"
+	};
+	var pathname = window.location.pathname;
+	for (var key in linkAssociations) {
+		if (pathname.indexOf(key) !== -1) {
+			if (linkAssociations[key] === null) {
+				break;
+			}
+			document.querySelector("header .links ." + linkAssociations[key]).classList.add("current");
 			break;
 		}
-		document.querySelector("header .links ." + linkAssociations[key]).classList.add("current");
-		break;
 	}
-}
 
-//Show admin console link if user has previously logged in
-if (localStorage.getItem("adminAccess") === "true") {
-	var adminLink = document.querySelector("header .links .admin");
-	adminLink.classList.remove("hidden");
-	adminLink.addEventListener("contextmenu", function (e) {
-		e.preventDefault();
-		contextMenu.present({
-			x: e.clientX,
-			y: e.clientY,
-			items: [
-				{
-					icon: "home",
-					label: "Admin Home",
-					callback: function () {
-						window.location.href = "/admin";
+	//Show admin console link if user has previously logged in
+	if (localStorage.getItem("adminAccess") === "true") {
+		var adminLink = document.querySelector("header .links .admin");
+		adminLink.classList.remove("hidden");
+		adminLink.addEventListener("contextmenu", function (e) {
+			e.preventDefault();
+			contextMenu.present({
+				x: e.clientX,
+				y: e.clientY,
+				items: [
+					{
+						icon: "home",
+						label: "Admin Home",
+						callback: function () {
+							window.location.href = "/admin";
+						}
+					},
+					{
+						icon: "database",
+						label: "Database",
+						callback: function () {
+							window.location.href = "/admin/database";
+						}
+					},
+					{
+						icon: "file-document",
+						label: "News List",
+						callback: function () {
+							window.location.href = "/admin/newsList";
+						}
+					},
+					{
+						icon: "play-list-search",
+						label: "Audit Log",
+						callback: function () {
+							window.location.href = "/admin/audit-log";
+						}
+					},
+					{
+						icon: "file-add",
+						label: "New Item",
+						type: "active",
+						callback: function () {
+							showDatabaseItemEditor();
+						}
+					},
+					{
+						icon: "file-document",
+						label: "New Article",
+						type: "active",
+						callback: function () {
+							showArticleEditor();
+						}
 					}
-				},
-				{
-					icon: "database",
-					label: "Database",
-					callback: function () {
-						window.location.href = "/admin/database";
-					}
-				},
-				{
-					icon: "file-document",
-					label: "News List",
-					callback: function () {
-						window.location.href = "/admin/newsList";
-					}
-				},
-				{
-					icon: "play-list-search",
-					label: "Audit Log",
-					callback: function () {
-						window.location.href = "/admin/audit-log";
-					}
-				},
-				{
-					icon: "file-add",
-					label: "New Item",
-					type: "active",
-					callback: function () {
-						showDatabaseItemEditor();
-					}
-				},
-				{
-					icon: "file-document",
-					label: "New Article",
-					type: "active",
-					callback: function () {
-						showArticleEditor();
-					}
-				}
-			],
+				],
+			});
 		});
-	});
+	}
 }
 
 //Listen for the Konami code
