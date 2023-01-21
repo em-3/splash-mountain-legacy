33c4d5418a27df0cd9acd1fccd9eb40ac224e158
cadehunter <60188691+cadehunter@users.noreply.github.com>
2023-01-21 13:03:23 -0700

    Added Konami Color Scheme

--- js/main.js
+++ js/main.js
@@ -146,18 +146,3 @@ if (document.querySelector("header .links")) {
 		});
 	}
 }
-
-//Listen for the Konami code
-var konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-var konamiCodeIndex = 0;
-
-document.addEventListener("keydown", function (event) {
-	if (event.keyCode === konamiCode[konamiCodeIndex]) {
-		konamiCodeIndex++;
-	} else {
-		konamiCodeIndex = 0;
-	}
-	if (konamiCodeIndex === konamiCode.length) {
-		window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-	}
-});
