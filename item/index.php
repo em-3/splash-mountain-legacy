eea7ea5d83d784d29c13fa0eb63d68569c834270
em-3 <69275008+em-3@users.noreply.github.com>
2024-10-13 15:29:34 -0400

    Removed Google Analytics

--- item/index.php
+++ item/index.php
@@ -22,15 +22,6 @@ require_once __DIR__ . "/../scripts/viewer_meta.php";
     <link rel="stylesheet" href="/css/global.css">
     <link rel="stylesheet" href="../styles.css">
 
-    <!-- Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-9N1M0HPYP7"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
-        gtag('config', 'G-9N1M0HPYP7');
-    </script>
-
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
